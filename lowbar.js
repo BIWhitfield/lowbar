b411d47bf36f27f2081fb437a08e69a195d09159
BIWhitfield <beniwhitfield@gmail.com>
2017-08-07 13:50:08 +0100

    [_some] implement and test _some

--- lowbar.js
+++ lowbar.js
@@ -180,6 +180,21 @@ _.every = function (list, predicate) {
   return true;
 };
 
+
+_.some = function (list, predicate) {
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      if (predicate(list[i])) return true;
+    }
+  } else if (typeof list === 'object') {
+    for (const key in list) {
+      if (predicate(list[key])) return true;
+    }
+  }
+  return false;
+};
+
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
