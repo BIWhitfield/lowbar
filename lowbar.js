f7c4bd54cade89827abea6f43e5bc95c1cd26141
BIWhitfield <beniwhitfield@gmail.com>
2017-08-07 13:57:08 +0100

    [_defaults] implement and test _defaults

--- lowbar.js
+++ lowbar.js
@@ -199,6 +199,16 @@ _.extends = function (destination, sources) {
   return Object.assign(destination, sources);
 };
 
+
+_.defaults = function (objects, defaults) {
+  for (const key in defaults) {
+    if (!objects.hasOwnProperty(key)) {
+      objects[key] = defaults[key];
+    }
+  }
+  return objects;
+};
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
