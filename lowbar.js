db96272f5c626affc6059e6d2d728a04b787cf34
Ben <beniwhitfield@gmail.com>
2017-08-07 14:08:51 +0100

    Uniq (#7)
    * [_identity] implement and test _identity
    
    * [_first] implement and test _first
    
    * [_last] implement and test _last
    
    * [_each] implement and test _each
    
    * [_indexOf] implement and test _indexOf
    
    * [_filter] implement and test _filter
    
    * [_reject] implement and test _reject
    
    * [_uniq] implement and test _uniq

--- lowbar.js
+++ lowbar.js
@@ -97,6 +97,20 @@ _.reject = function (array, func) {
 };
 
 
+_.uniq = function (array) {
+  array = array || [];
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    if (newArray.indexOf(value) === -1) {
+      newArray.push(value);
+    }
+  }
+  return newArray;
+};
+
+
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
