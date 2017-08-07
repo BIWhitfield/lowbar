a09d602a56e70b0146ca1aea3aa3f3d39676f91b
BIWhitfield <beniwhitfield@gmail.com>
2017-08-07 12:50:26 +0100

    [_identity] implement and test _identity

--- lowbar.js
+++ lowbar.js
@@ -1,5 +1,7 @@
 const _ = {};
 
+_.identity = value => value;
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -9,4 +9,23 @@ describe('_', () => {
   it('is an object', () => {
     expect(_).to.be.an('object');
   });
+
+  describe('#identity', () => {
+    it('is a function', () => {
+      expect(_.identity).to.be.a('function');
+    });
+    it('should return the original value if given a number', () => {
+      const result = _.identity(300);
+      expect(result).to.equal(300);
+    });
+    it('should return the original value if given a string', () => {
+      const result = _.identity('Hello World');
+      expect(result).to.equal('Hello World');
+    });
+    it('should return the original value if given an array', () => {
+      const input = [1, 2, 3, 4];
+      const result = _.identity(input);
+      expect(result).to.equal(input);
+    });
+  });
 });
